efd1ceabb58aed262303cd84fd1c834d12b9742a
Hariharan Sreedhar <hariharan.sreedhar@solitontech.com>
2023-07-20 14:30:07 +0530

    Added assignment 1 solution

---
+++ Demo1/Demo1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo1
+{
+    internal class Program
+    {
+        // Main method to call calculator functions
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Creating new calculator object
+            Calculator calci = new Calculator();
+            Console.WriteLine(calci.Addition(5, 5));
+            Console.WriteLine(calci.Subtraction(5, 5));
+            Console.WriteLine(calci.Multiplication(5, 5));
+            Console.WriteLine(calci.Division(5, 5));
+        }
+    }
+}
